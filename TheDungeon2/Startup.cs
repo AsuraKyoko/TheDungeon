8207e1e934968e6032181b99e7d2eba916cca1be
AsuraKyoko <steve.m.mahar@gmail.com>
2017-06-19 16:01:50 -0400

    Add project files.

---
+++ TheDungeon2/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheDungeon2.Startup))]
+namespace TheDungeon2
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
